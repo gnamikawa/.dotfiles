3d05ec004de0adb936048f240055a2037e77ecd0
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2023-07-13 11:14:29 +0900

    feat: add emmet configuration for nvim config

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -191,6 +191,36 @@ return {
     keys = {
       { "<leader>gw", ":Gitsigns toggle_word_diff<CR>", desc = "Toggle inline word diff", mode = "n" , noremap = true },
       { "<leader>gl", ":Gitsigns toggle_linehl<CR>", desc = "Toggle inline line diff", mode = "n" , noremap = true },
+    },
+    {
+      "olrtg/emmet-language-server",
+      config = function ()
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = "astro,css,eruby,html,htmldjango,javascriptreact,less,pug,sass,scss,svelte,typescriptreact,vue",
+        callback = function()
+          vim.lsp.start({
+            cmd = { "emmet-language-server", "--stdio" },
+            root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
+            init_options = {
+              --- @type table<string, any> https://docs.emmet.io/customization/preferences/
+              preferences = {},
+              --- @type "always" | "never" defaults to `"always"`
+              showexpandedabbreviation = "always",
+              --- @type boolean defaults to `true`
+              showabbreviationsuggestions = true,
+              --- @type boolean defaults to `false`
+              showsuggestionsassnippets = false,
+              --- @type table<string, any> https://docs.emmet.io/customization/syntax-profiles/
+              syntaxprofiles = {},
+              --- @type table<string, string> https://docs.emmet.io/customization/snippets/#variables
+              variables = {},
+              --- @type string[]
+              excludelanguages = {},
+            },
+          })
+        end,
+      })
+      end,
     }
   }
 }
