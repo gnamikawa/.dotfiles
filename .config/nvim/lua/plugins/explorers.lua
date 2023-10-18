69e076f49bf4f9099ffcf386d3c4d1caeb48e10a
Genzo <genzo.namikawa@outlook.com>
2023-10-19 02:08:47 +0900

    tweak: open full screen file explorer instead of sidebar

--- .config/nvim/lua/plugins/explorers.lua
+++ .config/nvim/lua/plugins/explorers.lua
@@ -23,6 +23,16 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        window = {
+          position = "current",
+        },
+      })
+    end,
   },
 
   {
