0327e46247f930f38e11981aeae3458306289326
Genzo <genzo.namikawa@outlook.com>
2023-10-18 23:20:49 +0900

    style: remove comment

--- .config/nvim/lua/plugins/stray.lua
+++ .config/nvim/lua/plugins/stray.lua
@@ -1,12 +1,3 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-
--- every spec file under config.plugins will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
 
   -- use mini.starter instead of alpha
