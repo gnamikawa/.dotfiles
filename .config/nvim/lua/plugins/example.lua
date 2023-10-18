95e818bdb1bcc6360c1281956cb5e1a9b7141e6d
Genzo <genzo.namikawa@outlook.com>
2023-10-18 23:01:27 +0900

    fix: suda_smart_edit not run in time
    This global flag needs to be set earlier in the lazy.vim lifecycle for the autocmd to be created; config is too slow, and init runs before config.

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -50,8 +50,8 @@ return {
   },
   {
     "lambdalisue/suda.vim",
-    config = function()
-      vim.g.suda_smart_edit = true
+    init = function()
+      vim.g.suda_smart_edit = 1
     end
   },
   {
