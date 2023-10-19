1c370457ce6ee6157f0bd26691f805f43f9bc4ef
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2023-10-19 14:16:31 +0900

    tweak: change conflicting keybinding

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -10,7 +10,7 @@ return {
     },
     keys = {
       { "<leader>gw", ":Gitsigns toggle_word_diff<CR>", desc = "Toggle inline word diff", mode = "n", noremap = true },
-      { "<leader>gl", ":Gitsigns toggle_linehl<CR>", desc = "Toggle inline line diff", mode = "n", noremap = true },
+      { "<leader>gW", ":Gitsigns toggle_linehl<CR>", desc = "Toggle inline line diff", mode = "n", noremap = true },
     },
   },
 }
