56353cf885db0d6eabd5ba0418fe32a850cd1785
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2023-10-19 14:20:19 +0900

    feat: add gitlab integration

--- .config/nvim/lua/plugins/explorers.lua
+++ .config/nvim/lua/plugins/explorers.lua
@@ -53,4 +53,112 @@ return {
       },
     },
   },
+
+  {
+    "harrisoncramer/gitlab.nvim",
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+    },
+    build = function()
+      require("gitlab.server").build(true)
+    end,
+    config = function()
+      require("gitlab").setup()
+    end,
+    keys = {
+      {
+        "<leader>glr",
+        function()
+          require("gitlab").review()
+        end,
+        desc = "Gitlab review",
+      },
+      {
+        "<leader>gls",
+        function()
+          require("gitlab").review()
+        end,
+        desc = "Gitlab summary",
+      },
+      {
+        "<leader>glA",
+        function()
+          require("gitlab").approve()
+        end,
+        desc = "Gitlab approve",
+      },
+      {
+        "<leader>glR",
+        function()
+          require("gitlab").revoke()
+        end,
+        desc = "Gitlab revoke",
+      },
+      {
+        "<leader>glc",
+        function()
+          require("gitlab").create_comment()
+        end,
+        desc = "Gitlab create comment",
+      },
+      {
+        "<leader>gln",
+        function()
+          require("gitlab").create_note()
+        end,
+        desc = "Gitlab create note",
+      },
+      {
+        "<leader>gld",
+        function()
+          require("gitlab").toggle_discussions()
+        end,
+        desc = "Gitlab discussions",
+      },
+      {
+        "<leader>glaa",
+        function()
+          require("gitlab").add_assignee()
+        end,
+        desc = "Gitlab add assignee",
+      },
+      {
+        "<leader>glad",
+        function()
+          require("gitlab").delete_assignee()
+        end,
+        desc = "Gitlab delete assignee",
+      },
+      {
+        "<leader>glra",
+        function()
+          require("gitlab").add_reviewer()
+        end,
+        desc = "Gitlab add reviewer",
+      },
+      {
+        "<leader>glrd",
+        function()
+          require("gitlab").delete_reviewer()
+        end,
+        desc = "Gitlab delete reviewer",
+      },
+      {
+        "<leader>glp",
+        function()
+          require("gitlab").pipeline()
+        end,
+        desc = "Gitlab pipeline",
+      },
+      {
+        "<leader>glo",
+        function()
+          require("gitlab").open_in_browser()
+        end,
+        desc = "Gitlab open_in_browser",
+      },
+    },
+  },
 }
