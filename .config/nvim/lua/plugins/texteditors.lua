a311525cbc19a9f7c765d2939c810c5c6fc1fbd9
Genzo <genzo.namikawa@outlook.com>
2023-10-21 16:12:51 +0900

    fix: disable lazy loading for cutlass

--- .config/nvim/lua/plugins/texteditors.lua
+++ .config/nvim/lua/plugins/texteditors.lua
@@ -6,6 +6,7 @@ return {
 
   {
     "svermeulen/vim-cutlass",
+    lazy = false,
     keys = {
       { "x", "d", mode = "n", desc = "Cut", noremap = true },
       { "x", "d", mode = "v", desc = "Cut", noremap = true },
