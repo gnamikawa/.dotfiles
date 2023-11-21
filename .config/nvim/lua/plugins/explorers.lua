f56ed76736b1fd399c420eeb8e5de826d70394ab
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2023-11-21 09:16:43 +0900

    feat: add ranger

--- .config/nvim/lua/plugins/explorers.lua
+++ .config/nvim/lua/plugins/explorers.lua
@@ -206,4 +206,17 @@ return {
       },
     },
   },
+
+  {
+    "kelly-lin/ranger.nvim",
+    config = function()
+      require("ranger-nvim").setup({ replace_netrw = true })
+      vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+        noremap = true,
+        callback = function()
+          require("ranger-nvim").open(true)
+        end,
+      })
+    end,
+  },
 }
