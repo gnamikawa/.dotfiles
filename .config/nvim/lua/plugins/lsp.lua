241049bdbbd18f92a7712b79a51c4b1e3d279a53
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2024-03-10 15:20:14 +0900

    feat: add rename

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,12 +3,49 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-      table.insert(opts.sources, nls.builtins.formatting.stylelint)
-      table.insert(opts.sources, nls.builtins.formatting.shfmt)
-      table.insert(opts.sources, nls.builtins.formatting.remark)
-      table.insert(opts.sources, nls.builtins.formatting.qmlformat)
-      return opts
+
+      local dontusereally = {
+        method = nls.methods.DIAGNOSTICS,
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        generator = {
+          fn = function(params)
+            local diagnostics = {}
+
+            -- sources have access to a params object
+            -- containing info about the current file and editor state
+            for i, line in ipairs(params.content) do
+              local col, end_col = line:find("really")
+              if col and end_col then
+                -- null-ls fills in undefined positions
+                -- and converts source diagnostics into the required format
+                table.insert(diagnostics, {
+                  row = i,
+                  col = col,
+                  end_col = end_col + 1,
+                  source = "no-really",
+                  message = "Don't use really!",
+                  severity = vim.diagnostic.severity.WARN,
+                })
+              end
+            end
+            return diagnostics
+          end,
+        },
+      }
+
+      nls.register(dontusereally)
+
+      return {
+        sources = {
+          dontusereally,
+          nls.builtins.formatting.prettierd,
+          nls.builtins.code_actions.gitsigns,
+          nls.builtins.formatting.qmlformat,
+          nls.builtins.formatting.remark,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.code_actions.refactoring,
+        },
+      }
     end,
   },
 
--- .config/nvim/lua/plugins/system.lua
+++ .config/nvim/lua/plugins/system.lua
@@ -1,7 +1,9 @@
 return {
   {
     "folke/noice.nvim",
+
     lazy = false,
+
     dependencies = {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
@@ -10,6 +12,13 @@ return {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
     },
+
+    opt = {
+      presets = {
+        inc_rename = true,
+      },
+    },
+
     config = {
       cmdline = {
         view = "cmdline_popup",
@@ -25,8 +34,28 @@ return {
       },
     },
   },
+
+  {
+    "smjonas/inc-rename.nvim",
+
+    config = function()
+      require("inc_rename").setup()
+    end,
+    keys = {
+      {
+        "<leader>cr",
+        ":IncRename " .. vim.fn.expand("<cword>"),
+        mode = "n",
+        desc = "Rename under cursor",
+        noremap = true,
+      },
+    },
+    lazy = false,
+  },
+
   {
     "lambdalisue/suda.vim",
+
     init = function()
       vim.g.suda_smart_edit = 1
     end,
