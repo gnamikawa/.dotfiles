bebe340f03ea29bc4fc2235f0138e06a01ac5f1c
Genzo <genzo.namikawa@outlook.com>
2024-07-22 03:15:17 +0900

    feat: enable basics

--- .config/nvim/lua/plugins/vscode.lua
+++ .config/nvim/lua/plugins/vscode.lua
@@ -9,11 +9,145 @@ return {
       { "X", "D", mode = "n", desc = "Cut line", noremap = true },
     },
   },
+
+  {
+    "nvimtools/none-ls.nvim",
+    event = "LazyFile",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.stylua,
+        nls.builtins.completion.spell,
+      })
+    end,
+  },
+
   {
     "Mofiqul/vscode.nvim",
+    priority = 1000,
     lazy = false,
+    config = {
+	transparent = true,
+    }
+  },
+  
+  {
+    "nvim-treesitter/nvim-treesitter",
+  },
+
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    config = {}
+  },
+
+  {
+    "folke/which-key.nvim",
+    lazy = false,
+    ---@class wk.Opts
+    opts = {
+      defaults = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["gs"] = { name = "+surround" },
+        ["z"] = { name = "+fold" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader><tab>"] = { name = "+tabs" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>c"] = { name = "+code" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>gh"] = { name = "+hunks" },
+        ["<leader>q"] = { name = "+quit/session" },
+        ["<leader>s"] = { name = "+search" },
+        ["<leader>u"] = { name = "+ui" },
+        ["<leader>w"] = { name = "+windows" },
+        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      },
+    },
+    config = function(_, opts)
+      vim.opt.timeoutlen = 200
+
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<cr>", mode = "n", desc = "Open explorer", noremap = true },
+    },
     config = function()
-      require("vscode").load("dark")
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        window = {
+          mappings = {
+            ["<space>"] = {
+              "toggle_node",
+              nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+            },
+            ["<2-LeftMouse>"] = "open",
+            ["l"] = "open",
+            ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+            -- ["P"] = { "toggle_preview", config = { use_float = true } },
+            ["S"] = "open_split",
+            ["s"] = "open_vsplit",
+            -- ["S"] = "split_with_window_picker",
+            -- ["s"] = "vsplit_with_window_picker",
+            ["t"] = "open_tabnew",
+            -- ["<cr>"] = "open_drop",
+            -- ["t"] = "open_tab_drop",
+            ["w"] = "open_with_window_picker",
+            -- ["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+            ["h"] = "close_node",
+            -- ['C'] = 'close_all_subnodes',
+            ["z"] = "close_all_nodes",
+            --["Z"] = "expand_all_nodes",
+            ["a"] = {
+              "add",
+              -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+              config = {
+                show_path = "none", -- "none", "relative", "absolute"
+              },
+            },
+            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+            ["d"] = "delete",
+            ["r"] = "rename",
+            ["y"] = "copy_to_clipboard",
+            ["x"] = "cut_to_clipboard",
+            ["p"] = "paste_from_clipboard",
+            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+            -- ["c"] = {
+            --  "copy",
+            --  config = {
+            --    show_path = "none" -- "none", "relative", "absolute"
+            --  }
+            --}
+            ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+            ["q"] = "close_window",
+            ["R"] = "refresh",
+            ["?"] = "show_help",
+            ["<"] = "prev_source",
+            [">"] = "next_source",
+            ["i"] = "show_file_details",
+          },
+        },
+      })
     end,
   },
 }
