4142bd83e5d19db1f17cad04eb3103dce2200a2a
Genzo <genzo.namikawa@outlook.com>
2024-07-24 01:55:47 +0900

    feat: enable marks

--- .config/nvim/lua/old.plugins/texteditors.lua
+++ .config/nvim/lua/old.plugins/texteditors.lua
@@ -1,34 +1,6 @@
 -- Plugins that influence keybinds that are frequently used to edit text.
 
 return {
-  { "echasnovski/mini.pairs", enabled = false },
-  { "chentoast/marks.nvim" },
-
-  {
-    "svermeulen/vim-cutlass",
-    lazy = false,
-    keys = {
-      { "x", "d", mode = "n", desc = "Cut", noremap = true },
-      { "x", "d", mode = "v", desc = "Cut", noremap = true },
-      { "xx", "dd", mode = "n", desc = "Cut line", noremap = true },
-      { "X", "D", mode = "n", desc = "Cut line", noremap = true },
-    },
-  },
-
-  {
-    "fedepujol/move.nvim",
-    keys = {
-      { "<A-up>", "<cmd>MoveLine(-1)<CR>", desc = "Move line area down", noremap = true },
-      { "<A-down>", "<cmd>MoveLine(1)<CR>", desc = "Move line area up", noremap = true },
-      { "<A-right>", "<cmd>MoveWord(1)<CR>", desc = "Move word forward", noremap = true },
-      { "<A-left>", "<cmd>MoveWord(-1)<CR>", desc = "Move word backward", noremap = true },
-      { "<A-left>", ":MoveHBlock(-1)<CR>", desc = "Move visual area left", mode = "v", noremap = true },
-      { "<A-down>", ":MoveBlock(1)<CR>", desc = "Move visual area down", mode = "v", noremap = true },
-      { "<A-up>", ":MoveBlock(-1)<CR>", desc = "Move visual area up", mode = "v", noremap = true },
-      { "<A-right>", ":MoveHBlock(1)<CR>", desc = "Move visual area right", mode = "v", noremap = true },
-    },
-  },
-
   {
     "kevinhwang91/nvim-ufo",
     event = "LspAttach",
