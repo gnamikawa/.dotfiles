03e45c69b4b1a430973b62984cc49a9076c56481
Genzo <genzo.namikawa@outlook.com>
2024-07-24 01:33:59 +0900

    feat: add modern ui elements

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "folke/noice.nvim",
+        lazy = false,
+        keys = {
+            { "<leader>sn",  "",                                                                            desc = "+noice" },
+            { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                              desc = "Redirect Cmdline" },
+            { "<leader>snl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
+            { "<leader>snh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
+            { "<leader>sna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
+            { "<leader>snd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
+            { "<leader>snt", function() require("noice").cmd("pick") end,                                   desc = "Noice Picker (Telescope/FzfLua)" },
+            { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,                           expr = true,              desc = "Scroll Forward",  mode = { "i", "n", "s" } },
+            { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,                           expr = true,              desc = "Scroll Backward", mode = { "i", "n", "s" } },
+        },
+        config = {
+            messages = {
+                enabled = true,        -- enables the Noice messages UI
+                view = "mini",         -- default view for messages
+                view_error = "mini",   -- view for errors
+                view_warn = "mini",    -- view for warnings
+                view_history = "mini", -- view for :messages
+                view_search = "mini",  -- view for search count messages. Set to `false` to disable
+            },
+            notify = {
+                view = "mini",
+            },
+        }
+    }
+}
