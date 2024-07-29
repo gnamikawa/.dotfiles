e3969c33f7bbd6e9fee9b92feb2e0c6070ed711a
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2024-07-29 11:02:12 +0900

    fix: configure themes

--- .config/nvim/lua/plugins/vscode.lua
+++ .config/nvim/lua/plugins/vscode.lua
@@ -3,8 +3,13 @@ return {
     "Mofiqul/vscode.nvim",
     priority = 1000,
     lazy = false,
-    config = {
-      transparent = true,
-    },
+    config = {},
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = {},
   },
 }
