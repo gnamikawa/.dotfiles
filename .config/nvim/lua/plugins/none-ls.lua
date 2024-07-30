e1fe312eff8ec952a1bd6019702a1f8b84bded9f
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2024-07-30 15:39:14 +0900

    feat: enable nullls configs

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -8,10 +8,30 @@ return {
       null_ls.setup({
         debug = true,
         sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.completion.spell,
+          -- TODO: Get rid of this after mason-null-ls is working.
+
+          -- golang
+          null_ls.builtins.formatting.gofmt,
+
+          -- python
           null_ls.builtins.formatting.black,
+          null_ls.builtins.diagnostics.pylint,
+          null_ls.builtins.formatting.isort,
+
+          -- markdown
+          null_ls.builtins.formatting.remark,
+
+          -- qml
+          null_ls.builtins.formatting.qmlformat,
+
+          -- etc
+          null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.formatting.shfmt,
+          null_ls.builtins.code_actions.refactoring,
+          null_ls.builtins.code_actions.impl,
+          -- null_ls.builtins.code_actions.ts_node_action,
+          null_ls.builtins.hover.printenv,
+          null_ls.builtins.hover.dictionary,
         },
         on_attach = function(client, bufnr)
           local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
