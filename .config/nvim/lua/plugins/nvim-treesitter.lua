1990392feca0c189500816597a119a1b361a9d7b
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2024-08-06 13:53:28 +0900

    fix: treesitter highlight and overall functionality not working on startup

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -11,33 +11,35 @@ return {
     keys = {
       -- { "<c-space>", desc = "Increment Selection" },
       -- { "<bs>", desc = "Decrement Selection", mode = "x" },
-      { "gd", function() vim.lsp.buf.definition() end, desc = "Go to definition", mode = "n" },
-      { "gr" , "<cmd>Telescope lsp_references<cr>", mode = "n"}
+      { "gd",  function() vim.lsp.buf.definition() end, desc = "Go to definition", mode = "n" },
+      { "gr",  "<cmd>Telescope lsp_references<cr>",     mode = "n" }
     },
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    config = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      -- incremental_selection = {
-      --   enable = true,
-      --   keymaps = {
-      --     init_selection = "<C-space>",
-      --     node_incremental = "<C-space>",
-      --     scope_incremental = false,
-      --     node_decremental = "<bs>",
-      --   },
-      -- },
-
-      textobjects = {
-        move = {
+    config = function()
+      ---@type TSConfig
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
           enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
         },
-      },
-    },
+        textobjects = {
+          enable = true,
+          move = {
+            enable = true,
+            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+          },
+        },
+      })
+    end,
   },
 }
