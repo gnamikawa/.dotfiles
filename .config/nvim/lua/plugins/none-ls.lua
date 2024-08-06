9334a64245da396a07ead273fe2a357a8c052194
Genzo Namikawa <genzo.namikawa@mujin.co.jp>
2024-08-06 13:59:44 +0900

    fix: update and disable pylint

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -15,7 +15,7 @@ return {
 
           -- python
           null_ls.builtins.formatting.black,
-          null_ls.builtins.diagnostics.pylint,
+          -- null_ls.builtins.diagnostics.pylint,
           null_ls.builtins.formatting.isort,
 
           -- markdown
